332e2e8e72ee580155d95f22c4d11137f1fc5cc8
rpdev <rpdev>
2013-04-13 16:06:43 +0200

    Added frame for adding new berry, minor improvments of PriceGui

--- Strawberry/src/froapp3/Gui.java
+++ Strawberry/src/froapp3/Gui.java
@@ -3,16 +3,22 @@ package froapp3;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EnumMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.TableRowSorter;
+
+import froapp3.Database.Berries;
 
 class Gui extends JFrame {
 	private static final long serialVersionUID = 800976508415127289L;
@@ -24,6 +30,19 @@ class Gui extends JFrame {
 		final JTable table = new JTable(tableModel);
 		
 		JButton add = new JButton("Lägg till");
+		add.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					
+					@Override
+					public void run() {
+						new AddBerry(tableModel);
+					}
+				});
+			}
+		});
 		JButton show = new JButton("Visa");
 		show.addActionListener(new ActionListener() {
 			
@@ -34,8 +53,11 @@ class Gui extends JFrame {
 					@Override
 					public void run() {
 						int row = table.getSelectedRow();
-						if(row > -1)
-							new PriceGui(tableModel.getPriceTable((int) tableModel.getValueAt(row, 0)), "Title");
+						if(row > -1){
+							int id = (int) tableModel.getValueAt(row, Berries.ID.ordinal());
+							String title = (String) tableModel.getValueAt(row, Berries.NAME.ordinal());
+							new PriceGui(tableModel.getPriceTable(id), title);
+						}
 					}
 				});
 			}
@@ -56,6 +78,44 @@ class Gui extends JFrame {
 		this.pack();
 		this.setVisible(true);
 	}
+	
+	private class AddBerry extends JFrame{
+		private static final long serialVersionUID = 5580159001584032476L;
+		private final EnumMap<Berries, JTextField> fields = new EnumMap<>(Berries.class);
+		
+		private AddBerry(TableBackend tableModel){
+			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			this.setLayout(new GridLayout(0,2));
+			this.setTitle("Nytt bär");
+			this.setLocationRelativeTo(null);
+			Berries[] values = Berries.values();
+			for(int i=1;i<values.length;i++){ // skip ID, auto generated
+				this.add(new JLabel(values[i].getName()));
+				JTextField field = new JTextField();
+				fields.put(values[i], field);
+				this.add(field);
+			}
+			JButton save = new JButton("Spara");
+			JButton abort = new JButton("Avbryt");
+			abort.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					EventQueue.invokeLater(new Runnable() {
+						
+						@Override
+						public void run() {
+							dispose();
+						}
+					});
+				}
+			});
+			this.add(save);
+			this.add(abort);
+			this.pack();
+			this.setVisible(true);
+		}
+	}
 
 	private class PriceGui extends JFrame{
 		private static final long serialVersionUID = -1224970052450584412L;
@@ -64,6 +124,7 @@ class Gui extends JFrame {
 			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			this.setTitle(title);
 			this.setLayout(new BorderLayout());
+			this.setLocationRelativeTo(Gui.this);
 			
 			JButton add = new JButton("Lägg till");
 			JButton remove = new JButton("Radera");
